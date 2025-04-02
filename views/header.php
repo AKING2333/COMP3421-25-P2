23192b3080eac12405317569956aa71d76fdb284
AKING2333 <13266011666@163.com>
2025-04-02 18:02:52 +0800

    use web store template

---
+++ views/header.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Store</title>
+
+    <!--  CSS  -->
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <link href="/assets/css/style.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="https://cdn.sellix.io/static/css/embed.css">
+    <link rel="stylesheet" href="/../assets/css/colors.css">
+    <link rel="stylesheet" href="/../assets/css/general.css">
+    <link rel="stylesheet" href="/../assets/css/navbar.css">
+    <link rel="stylesheet" href="/../assets/css/header.css">
+    <link rel="stylesheet" href="/../assets/css/about.css">
+    <link rel="stylesheet" href="/../assets/css/shop.css">
+    <link rel="stylesheet" href="/../assets/css/faq.css">
+    <link rel="stylesheet" href="/../assets/css/reviews.css">
+    <link rel="stylesheet" href="/../assets/css/footer.css">
+
+
+</head>
+<body>
+
+
+
+<div class="section__navbar">
+
+    <div class="navbar">
+
+        <div class="navbar__header">
+            <a href="" class="navbar__brand">
+                Online Store
+            </a>
+
+            <div class="navbar__menu" onclick="navbar()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="14" viewBox="0 0 20 14">
+                    <g id="menu" transform="translate(-2 -5)">
+                        <line id="Line_1" data-name="Line 1" x2="15" transform="translate(6 12)" fill="none"
+                                stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                        <line id="Line_2" data-name="Line 2" x2="18" transform="translate(3 6)" fill="none"
+                                stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                        <line id="Line_3" data-name="Line 3" x2="18" transform="translate(3 18)" fill="none"
+                                stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    </g>
+                </svg>
+            </div>
+        </div>
+
+        <div class="navbar__nav" id="navbarNav">
+
+            <ul>
+                <a href="index.html" class="navbar__href">
+                    <li class="navbar__link active">Home</li>
+                </a>
+                <a href="index.html#about" class="navbar__href">
+                    <li class="navbar__link">About</li>
+                </a>
+                <a href="index.html#shop" class="navbar__href">
+                    <li class="navbar__link">Shop</li>
+                </a>
+                <a href="index.html#faq" class="navbar__href">
+                    <li class="navbar__link">FAQ</li>
+                </a>
+                <a href="index.html#reviews" class="navbar__href">
+                    <li class="navbar__link">Reviews</li>
+                </a>
+                <a href="terms.html" class="navbar__href">
+                    <li class="navbar__link">Terms</li>
+                </a>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+</body>
+</html>
