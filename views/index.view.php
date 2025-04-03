68c195fe2557a8a7a907b8739e8fafba6ece0114
AKING2333 <13266011666@163.com>
2025-04-03 15:38:07 +0800

    use PolyU style

--- views/index.view.php
+++ views/index.view.php
@@ -1,5 +1,52 @@
 <?php require_once __DIR__ . '/header.php'; ?>
 
+<div class="section__header animation">
+
+<div class="header__circle">
+    <div class="circle circle__one"></div>
+    <div class="circle circle__two"></div>
+    <div class="circle circle__three"></div>
+</div>
+
+<div class="header__content">
+
+    <h1 class="header__title">
+        PolyU
+        <span>store</span>
+    </h1>
+
+    <p class="header__description">
+    Welcome to the PolyU Store! Here, you can find clothing, accessories, 
+    and other items that show your pride in The Hong Kong Polytechnic University. 
+    Whether you're a student, alumni, or just visiting, the store has something special for everyone.
+    </p>
+
+    <div class="header__info">
+        <div class="header__square">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="22" viewBox="0 0 18 22">
+                <path id="shield" d="M12,22s8-4,8-10V5L12,2,4,5v7C4,18,12,22,12,22Z"
+                      transform="translate(-3 -1)" fill="none" stroke="#cf9816" stroke-linecap="round"
+                      stroke-linejoin="round" stroke-width="2"/>
+            </svg>
+        </div>
+        Safe and checked store
+    </div>
+
+    <a href="#about" class="header__arrow">
+        <svg class="header__arrow__icon" xmlns="http://www.w3.org/2000/svg" width="18.949" height="21.759" viewBox="0 0 18.949 21.759">
+            <g id="chevrons-down" transform="translate(-5.586 -4.586)">
+                <path id="Path_1" data-name="Path 1" d="M7,13l8.06,8.06L23.121,13" transform="translate(0 4.285)" fill="none" stroke="#4a4a4a" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                <path id="Path_2" data-name="Path 2" d="M7,6l8.06,8.06L23.121,6" fill="none" stroke="#4a4a4a" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+            </g>
+        </svg>
+
+        To Products List
+    </a>
+
+</div>
+
+</div>
+
 <div id="about" class="section__about animation">
 
     <div class="about__content">
